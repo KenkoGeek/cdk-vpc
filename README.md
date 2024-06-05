0742d1c1e071501b81804b002853d756816e8dce
Franklin Garcia <franklin.g.mendoza@gmail.com>
2024-06-05 16:49:29 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,72 @@
-# Welcome to your CDK TypeScript project
+## AWS CDK VPC Constructor
 
-This is a blank project for CDK development with TypeScript.
+This AWS Cloud Development Kit (CDK) construct provides a flexible and customizable way to provision a Virtual Private Cloud (VPC) within your AWS environment. It supports both non production and production configurations, allowing you to tailor network settings, VPC endpoints, and security measures to your project's specific needs.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+**Key Features**
 
-## Useful commands
+* **Configurable Subnets:** Easily define public and private subnets across multiple Availability Zones (AZs) and layers (e.g., app, database, transit gateway).
+* **Transit Gateway Integration:** Optionally connect your VPC to an existing Transit Gateway for seamless communication across AWS accounts, premises and regions.
+* **VPC Endpoints:** Enable secure and private access to AWS services like S3, DynamoDB, SSM, SSM Messages, and EC2.
+* **Flow Logs:** Capture VPC network traffic data for monitoring and troubleshooting.
+* **Environment-Specific Configuration:** Use the `cdk.json` file to manage settings for different environments (development, testing, production).
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+**Prerequisites**
+
+* **AWS CDK:** Make sure you have the AWS CDK installed and configured. Refer to the [AWS CDK documentation](https://docs.aws.amazon.com/cdk/v2/guide/getting_started.html) for setup instructions.
+* **TypeScript:** This project is built with TypeScript. You'll need a TypeScript development environment.
+* **AWS Account:** You'll need an AWS account to deploy the VPC stack.
+
+**Getting Started**
+
+1. **Clone the Repository:**
+   ```bash
+   git clone <your-repository-url>
+   cd <your-repository-directory>
+   ```
+
+2. **Install Dependencies:**
+   ```bash
+   npm install
+   ```
+
+3. **Configure Environments (cdk.json):**
+   * Modify the `cdk.json` file to match your project's requirements. Customize the VPC CIDR block, subnet masks, transit gateway ID (if used), VPC endpoint settings, and tags. You can add or remove environments.
+
+4. **Deploy:**
+   ```bash
+   cdk deploy --context envName=dev
+   ```
+   This will synthesize and deploy your VPC stack to the specified environment.
+
+**Destroy Resources**
+
+1. **Eliminate the resources:**
+   ```bash
+   cdk destroy --all --force --context envName=dev
+   ```
+   This will destroy all resources associated with the stack in the specified environment.
+
+**How It Works**
+
+This CDK project defines a stack (`CdkVpcStack`) that encapsulates the creation of the VPC and associated resources. The `cdk.json` file drives the configuration based on the environment you choose during deployment.
+
+**Customization (cdk.json)**
+
+| Parameter                           | Description                                                                                                                                 | Default Value                                   |
+|-------------------------------------|---------------------------------------------------------------------------------------------------------------------------------------------|------------------------------------------------|
+| `project_name`                      | The name of your project. Used for resource naming and tagging.                                                                             | `"my-project"`                                 |
+| `vpc_cidr_block`                    | The CIDR block for your VPC.                                                                                                                | `"10.1.0.0/20"`                                |
+| `az_count`                          | The number of Availability Zones to use.                                                                                                     | `2`                                            |
+| `nat_gateway_count`                 | The number of NAT gateways to create (0 if using a Transit Gateway).                                                                         | `1`                                            |
+| `use_transit_gateway`               | Set to `true` to use a existent Transit Gateway.                                                                                                      | `true`                                         |
+| `transit_gateway_id`                | The ID of your Transit Gateway (if you're using one). `use_transit_gateway` must be set to `true`.                                                                                     | `""`                      |
+| `create_public_subnets`             | Set to `true` to create public subnets in addition to private subnets.                                                                       | `true`                                         |
+| `public_subnet_mask_bits`           | The bitmask for your public subnets.                                                                                                         | `27`                                           |
+| `public_subnet_with_tgw_enabled`    | Set to `true` to route traffic from public subnets through the Transit Gateway.                                                              | `false`                                        |
+| `subnet_layers`                     | Define private subnet layers names with their bitmasks (e.g., `{"app": 23, "db": 27, "tgw": 28}`).                                                  | `{"app": 23, "db": 27, "tgw": 28}`             |
+| `encryption_key`                    | The ARN of the KMS key to use for flow log encryption (leave empty to create a new key).                                                     | `""`                                           |
+| `s3_vpc_endpoint_enabled`           | Set to `true` to create a gateway S3 VPC endpoint.                                                                                                  | `true`                                         |
+| `dynamodb_vpc_endpoint_enabled`     | Set to `true` to create a gateway DynamoDB VPC endpoint.                                                                                            | `true`                                         |
+| `session_manager_vpc_endpoints_enabled` | Set to `false` to not create interfaces VPC endpoints for SSM, SSM Messages, and EC2.                                                                    | `false`                                        |
+| `additional_cidrs`                  | An array of additional CIDR blocks to route through the Transit Gateway (e.g., `["192.168.0.0/16", "172.16.0.0/12"]`). If is empty will no create additional routes to VPC.                        | `[]`           |
+| `tags`                              | A dictionary of tags (key-value pairs) to apply to the VPC and its resources (e.g., `{"owner": "John Doe", "project": "my-project"}`).       | `{}`|
--- lib/cdk-vpc-stack.ts
+++ lib/cdk-vpc-stack.ts
@@ -1,16 +1,262 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import * as kms from 'aws-cdk-lib/aws-kms';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+interface CdkVpcStackProps extends cdk.StackProps {
+  envName: string;
+}
 
 export class CdkVpcStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: CdkVpcStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const environment = props.envName;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkVpcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Load variables from cdk.json
+    const config = this.node.tryGetContext(environment);
+
+    // VPC
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      ipAddresses: ec2.IpAddresses.cidr(config.vpc_cidr_block),
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      maxAzs: config.az_count,
+      natGateways: config.nat_gateway_count,
+      subnetConfiguration: config.create_public_subnets ? [ // Conditional subnet creation
+        {
+          cidrMask: config.public_subnet_mask_bits,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          mapPublicIpOnLaunch: false,
+        },
+        ...Object.entries(config.subnet_layers).map(([layerName, cidrMask]) => ({
+          cidrMask: parseInt(cidrMask as string, 10),
+          name: layerName,
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+        })),
+      ] : [
+        ...Object.entries(config.subnet_layers).map(([layerName, cidrMask]) => ({
+          cidrMask: parseInt(cidrMask as string, 10),
+          name: layerName,
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+        })),
+      ], 
+    });
+
+    let tgwAttachment: ec2.CfnTransitGatewayVpcAttachment | undefined;
+    const lastLayerName = Object.keys(config.subnet_layers)[Object.keys(config.subnet_layers).length - 1]; // Find last layer
+    const lastLayerSubnets = vpc.privateSubnets.filter(subnet => subnet.node.path.includes(lastLayerName)); // Select last layer subnets for TGW attachment
+    const subnets = vpc.selectSubnets(); // Find all the created subnets
+
+    if (config.use_transit_gateway) {
+      if (lastLayerSubnets.length === 0) {
+        throw new Error(`No subnets found for the last layer: ${lastLayerName}`);
+      }
+
+      tgwAttachment = new ec2.CfnTransitGatewayVpcAttachment(this, 'TgwAttachment', {
+        transitGatewayId: config.transit_gateway_id,
+        vpcId: vpc.vpcId,
+        subnetIds: lastLayerSubnets.map(subnet => subnet.subnetId),  // Use lastLayerSubnets
+      });
+      tgwAttachment.node.addDependency(vpc);
+
+      subnets.subnets.forEach((subnet, index) => { 
+        const routeTable = subnet.routeTable;
+        
+        const internetRoute = new ec2.CfnRoute(this, `TransitGatewayRoute${index}`, { 
+            routeTableId: routeTable.routeTableId,  
+            destinationCidrBlock: '0.0.0.0/0',
+            transitGatewayId: config.transit_gateway_id,
+          });
+          internetRoute.node.addDependency(tgwAttachment ?? this);
+
+          if (config.additional_cidrs) {
+            if (config.public_subnet_with_tgw_enabled) {
+              for (const subnet of vpc.publicSubnets.concat(vpc.privateSubnets)) { 
+                const routeTable = subnet.routeTable;
+                for (const [cidrIndex, cidr] of config.additional_cidrs.entries()) { 
+                  new ec2.CfnRoute(this, `AdditionalCidrRoute-${subnet.node.id}-${cidrIndex}`, {
+                    routeTableId: routeTable.routeTableId,
+                    destinationCidrBlock: cidr,  
+                    transitGatewayId: config.transit_gateway_id,
+                  }).node.addDependency(tgwAttachment ?? this); 
+                }
+              }
+            } else {
+              config.additional_cidrs.forEach((cidr: string, cidrIndex: number) => { 
+                const additionalCidrRoute = new ec2.CfnRoute(this, `AdditionalCidrRoute${index}-${cidrIndex}`, {
+                     routeTableId: routeTable.routeTableId,
+                     destinationCidrBlock: cidr,  
+                     transitGatewayId: config.transit_gateway_id,
+                 });
+                 additionalCidrRoute.node.addDependency(tgwAttachment ?? this); 
+             });
+            }
+          }
+      });
+    }
+
+    // Prettier names for VPC and Subnets
+    cdk.Tags.of(vpc).add("Name", `vpc-${config.project_name}-${environment}`);
+
+    vpc.publicSubnets.forEach((subnet) => {
+      const azIdentifier = subnet.availabilityZone.split('-').pop();
+      cdk.Tags.of(subnet).add("Name", `public-${config.project_name}-${environment}-${azIdentifier}`);
+    });
+    
+    vpc.privateSubnets.forEach((subnet) => {
+      const layerName = Object.keys(config.subnet_layers).find(
+        (layer) => subnet.node.id.includes(layer)
+      );
+      const azIdentifier = subnet.availabilityZone.split('-').pop();
+      if (layerName) {
+        cdk.Tags.of(subnet).add(
+          "Name",
+          `private-${config.project_name}-${environment}-${layerName}-${azIdentifier}`
+        );
+      } else {
+        console.warn(
+          `Unable to determine layer name for private subnet with ID: ${subnet.node.id}`
+        );
+      }
+    });
+
+    // VPC Endpoints
+    if (config.s3_vpc_endpoint_enabled) {
+      new ec2.GatewayVpcEndpoint(this, 'S3VpcEndpoint', {
+        vpc, service: ec2.GatewayVpcEndpointAwsService.S3
+      }).node.addDependency(vpc);
+    }
+    if (config.dynamodb_vpc_endpoint_enabled) {
+      new ec2.GatewayVpcEndpoint(this, 'DynamoDbVpcEndpoint', {
+        vpc, service: ec2.GatewayVpcEndpointAwsService.DYNAMODB, 
+      }).node.addDependency(vpc);
+    }
+    
+    if (config.session_manager_vpc_endpoints_enabled) {
+      const vpcEndpointSecurityGroup = new ec2.SecurityGroup(this, 'VpcEndpointSecurityGroup', {
+        vpc,
+        description: 'Security group for VPC endpoints',
+        allowAllOutbound: false,
+        securityGroupName: `vpc-endpoints-${config.project_name}-${environment}-sg`
+      });
+
+      vpcEndpointSecurityGroup.addIngressRule(
+        ec2.Peer.ipv4(vpc.vpcCidrBlock),
+        ec2.Port.tcp(443),
+        'Allow inbound HTTPS from VPC'
+      );
+
+      vpcEndpointSecurityGroup.addEgressRule(
+        ec2.Peer.ipv4(vpc.vpcCidrBlock),
+        ec2.Port.tcp(443),
+        'Allow outbound HTTPS to VPC'
+      );
+
+      new ec2.InterfaceVpcEndpoint(this, 'SsmEndpoint', {
+        vpc,
+        service: ec2.InterfaceVpcEndpointAwsService.SSM,
+        securityGroups: [vpcEndpointSecurityGroup],
+        privateDnsEnabled: true,
+        subnets: {
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        }
+      });
+      
+      new ec2.InterfaceVpcEndpoint(this, 'SsmMessagesEndpoint', {
+        vpc,
+        service: ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
+        securityGroups: [vpcEndpointSecurityGroup],
+        privateDnsEnabled: true,
+        subnets: {
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        }
+      });
+      
+      new ec2.InterfaceVpcEndpoint(this, 'Ec2Endpoint', {
+        vpc,
+        service: ec2.InterfaceVpcEndpointAwsService.EC2,
+        securityGroups: [vpcEndpointSecurityGroup],
+        privateDnsEnabled: true,
+        subnets: {
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        }
+      });
+    }
+
+    // Flow Logs
+    const flowLogRole = new iam.Role(this, 'FlowLogsRole', {
+      assumedBy: new iam.ServicePrincipal('logs.amazonaws.com'),
+    });
+
+    let flowLogKey: kms.IKey;
+
+    if (config.encryption_key) {
+      flowLogKey = kms.Key.fromKeyArn(this, 'FlowLogsEncryptionKey', config.encryption_key);
+    } else {
+      flowLogKey = new kms.Key(this, 'FlowLogsEncryptionKey', {
+        enableKeyRotation: true,
+        description: 'Used for cloudwatch logs encryption key',
+        alias: 'Logs',
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        policy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: [
+                "kms:Encrypt*",
+                "kms:Decrypt*",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:Describe*"
+              ],
+              resources: ['*'],
+              principals: [new iam.ServicePrincipal('logs.amazonaws.com')]
+            }),
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ["kms:*"],
+              resources: ['*'],
+              principals: [new iam.AccountPrincipal(this.account)]
+            })
+          ]
+        })
+      });
+    }
+
+    const flowLogLogGroup = new logs.LogGroup(this, 'FlowLogsLogGroup', {
+      retention: logs.RetentionDays.ONE_YEAR,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      logGroupName: `/vpc/flowlogs/${config.project_name}-${environment}`,
+      encryptionKey: flowLogKey
+    });
+    
+    new ec2.FlowLog(this, 'FlowLogs', {
+      resourceType: ec2.FlowLogResourceType.fromVpc(vpc),
+      destination: ec2.FlowLogDestination.toCloudWatchLogs(flowLogLogGroup, flowLogRole),
+      trafficType: ec2.FlowLogTrafficType.ALL
+    }).node.addDependency(vpc);
+
+    // Tags
+    const tags = config.tags;
+    for (const [key, value] of Object.entries(tags)) {
+      cdk.Tags.of(this).add(key, value as string);
+    }
+
+    // Outputs
+    new cdk.CfnOutput(this, "VPCId", {
+      description: "VPC ID",
+      value: vpc.vpcId,
+      exportName: `${this.stackName}-VPCId`
+    });
+
+    new cdk.CfnOutput(this, "VPCCidr", {
+      description: "VPC CIDR",
+      value: vpc.vpcCidrBlock,
+      exportName: `${this.stackName}-VPCCidr`
+    });
   }
 }
